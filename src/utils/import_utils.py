3739fa7a7da136db14ecb6cbf0a8796da5ddaa5e
Mattias Aabmets <mattias.aabmets@rik.ee>
2024-03-23 01:40:53 +0200

    Added file and import utilities

---
+++ src/utils/import_utils.py
@@ -0,0 +1,26 @@
+#
+#   European Union Public License 1.2
+#
+#   Copyright (c) 2024, Centre of Registers and Information Systems
+#
+#   The contents of this file are subject to the terms and conditions defined in the License.
+#   You may not use, modify, or distribute this file except in compliance with the License.
+#
+#   SPDX-License-Identifier: EUPL-1.2
+#
+from typing import Any
+from importlib import import_module
+
+
+__all__ = ["LazyImport"]
+
+
+class LazyImport:
+	def __init__(self, module_name: str):
+		self.module_name = module_name
+		self.module = None
+
+	def __getattr__(self, name: str) -> Any:
+		if self.module is None:
+			self.module = import_module(self.module_name)
+		return getattr(self.module, name)
