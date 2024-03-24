ed3af509e50dd862310d4d220661969d36de8c60
Mattias Aabmets <mattias.aabmets@rik.ee>
2024-03-24 23:01:38 +0200

    Fixed incorrect namespaces in header models

--- fastapi_xroad_soap/internal/envelope/header.py
+++ fastapi_xroad_soap/internal/envelope/header.py
@@ -16,27 +16,27 @@ from fastapi_xroad_soap.internal.envelope.base import Element, Attribute, BaseXm
 __all__ = ["XroadHeaderService", "XroadHeaderClient", "XroadHeader"]
 
 
-class XroadHeaderService(BaseXmlModel, tag="service", ns="xroad", nsmap=ENV_NSMAP):
-	object_type: str = Attribute(tag="objectType", ns="xid", default="SERVICE")
-	xroad_instance: str = Element(tag="xRoadInstance", ns="xid")
-	member_class: str = Element(tag="memberClass", ns="xid")
-	member_code: str = Element(tag="memberCode", ns="xid")
-	subsystem_code: t.Union[str, None] = Element(tag="subsystemCode", ns="xid", default=None)
-	service_code: str = Element(tag="serviceCode", ns="xid")
-	service_version: t.Union[str, None] = Element(tag="serviceVersion", ns="xid", default=None)
+class XroadHeaderService(BaseXmlModel, tag="service", ns="xro", nsmap=ENV_NSMAP, search_mode='unordered'):
+	object_type: str = Attribute(tag="objectType", ns="iden", default="SERVICE")
+	xroad_instance: str = Element(tag="xRoadInstance", ns="iden")
+	member_class: str = Element(tag="memberClass", ns="iden")
+	member_code: str = Element(tag="memberCode", ns="iden")
+	subsystem_code: t.Union[str, None] = Element(tag="subsystemCode", ns="iden", default=None)
+	service_code: str = Element(tag="serviceCode", ns="iden")
+	service_version: t.Union[str, None] = Element(tag="serviceVersion", ns="iden", default=None)
 
 
-class XroadHeaderClient(BaseXmlModel, tag="client", ns="xroad", nsmap=ENV_NSMAP):
-	object_type: str = Attribute(tag="objectType", ns="xid", default="SUBSYSTEM")
-	xroad_instance: str = Element(tag="xRoadInstance", ns="xid")
-	member_class: str = Element(tag="memberClass", ns="xid")
-	member_code: str = Element(tag="memberCode", ns="xid")
-	subsystem_code: t.Union[str, None] = Element(tag="subsystemCode", ns="xid", default=None)
+class XroadHeaderClient(BaseXmlModel, tag="client", ns="xro", nsmap=ENV_NSMAP, search_mode='unordered'):
+	object_type: str = Attribute(tag="objectType", ns="iden", default="SUBSYSTEM")
+	xroad_instance: str = Element(tag="xRoadInstance", ns="iden")
+	member_class: str = Element(tag="memberClass", ns="iden")
+	member_code: str = Element(tag="memberCode", ns="iden")
+	subsystem_code: t.Union[str, None] = Element(tag="subsystemCode", ns="iden", default=None)
 
 
-class XroadHeader(BaseXmlModel, tag="Header", ns="soapenv", nsmap=ENV_NSMAP):
-	proto_ver: str = Element(tag="protocolVersion", ns="xroad")
-	user_id: t.Union[str, None] = Element(tag="userId", ns="xroad", default=None)
-	id: str = Element(tag="id", ns="xroad")
+class XroadHeader(BaseXmlModel, tag="Header", ns="soapenv", nsmap=ENV_NSMAP, search_mode='unordered'):
+	user_id: t.Union[str, None] = Element(tag="userId", ns="xro", default=None)
+	proto_ver: str = Element(tag="protocolVersion", ns="xro")
+	id: str = Element(tag="id", ns="xro")
 	service: t.Union[XroadHeaderService, None] = None
 	client: XroadHeaderClient
