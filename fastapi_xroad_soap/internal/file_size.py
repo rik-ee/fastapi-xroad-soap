e766510991e785e96b81e9f0a00a6453eaa1fc6c
Mattias Aabmets <mattias.aabmets@gmail.com>
2024-04-01 03:56:54 +0300

    Added FileSize helper class

---
+++ fastapi_xroad_soap/internal/file_size.py
@@ -0,0 +1,58 @@
+#
+#   European Union Public License 1.2
+#
+#   Copyright (c) 2024, Centre of Registers and Information Systems
+#
+#   The contents of this file are subject to the terms and conditions defined in the License.
+#   You may not use, modify, or distribute this file except in compliance with the License.
+#
+#   SPDX-License-Identifier: EUPL-1.2
+#
+from abc import ABC, abstractmethod
+
+
+__all__ = [
+	"FileSize",
+	"FileSizeKB",
+	"FileSizeMB",
+	"FileSizeGB"
+]
+
+
+class FileSize(ABC):
+	def __init__(self, size: int = 1) -> None:
+		self.size = size
+
+	@property
+	@abstractmethod
+	def _multiplier(self) -> int: ...
+
+	@property
+	def value(self) -> int:
+		return self.size * self._multiplier
+
+	def __int__(self) -> int:
+		return self.value
+
+
+class FileSizeKB(FileSize):
+	@property
+	def _multiplier(self) -> int:
+		return 1024
+
+
+class FileSizeMB(FileSize):
+	@property
+	def _multiplier(self) -> int:
+		return 1024 ** 2
+
+
+class FileSizeGB(FileSize):
+	@property
+	def _multiplier(self) -> int:
+		return 1024 ** 3
+
+
+FileSize.KB = FileSizeKB
+FileSize.MB = FileSizeMB
+FileSize.GB = FileSizeGB
