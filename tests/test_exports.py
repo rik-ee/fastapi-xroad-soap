f7f3eebb49971f358e9fc4f3786cdc7a6e5d4eb0
Mattias Aabmets <mattias.aabmets@rik.ee>
2024-04-10 18:03:10 +0300

    Added pytest for testing library public interface imports

---
+++ tests/test_exports.py
@@ -0,0 +1,40 @@
+#
+#   European Union Public License 1.2
+#
+#   Copyright (c) 2024, Centre of Registers and Information Systems
+#
+#   The contents of this file are subject to the terms and conditions defined in the License.
+#   You may not use, modify, or distribute this file except in compliance with the License.
+#
+#   SPDX-License-Identifier: EUPL-1.2
+#
+from fastapi_xroad_soap import (
+	SoapService,
+	MessageBody,
+	XroadHeader
+)
+from fastapi_xroad_soap.elements import (
+	SwaRef
+)
+from fastapi_xroad_soap.faults import (
+	SoapFault,
+	ClientFault,
+	ServerFault
+)
+from fastapi_xroad_soap.utils import (
+	FileSize,
+	GlobalWeakStorage
+)
+
+
+__all__ = [
+	"SoapService",
+	"MessageBody",
+	"XroadHeader",
+	"SwaRef",
+	"SoapFault",
+	"ClientFault",
+	"ServerFault",
+	"FileSize",
+	"GlobalWeakStorage"
+]
