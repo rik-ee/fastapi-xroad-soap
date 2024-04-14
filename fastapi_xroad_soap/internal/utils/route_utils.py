7739d285fbade1330eb8ac502837bb771b8da2b8
Mattias Aabmets <mattias.aabmets@gmail.com>
2024-04-14 16:31:05 +0300

    Reduced cognitive complexity of the validate_annotations function

--- fastapi_xroad_soap/internal/utils/route_utils.py
+++ fastapi_xroad_soap/internal/utils/route_utils.py
@@ -32,36 +32,53 @@ def get_annotations(func: t.Any) -> dict:
 def validate_annotations(name: str, func: DecoratedCallable) -> dict:
 	anno = get_annotations(func)
 	for key, value in anno.items():
-		if key not in ["body", "header", "return"]:
-			raise ValueError(
-				f"Parameter name '{key}' not allowed for SOAP action {name}."
-				"\nOnly names 'body' and 'header' can be used for parameters."
-			)
-		elif key == "return":
-			if value is None:
-				continue
-			elif not isinstance(value, type) or not issubclass(value, MessageBody):
-				raise TypeError(
-					f"Return type annotation of the {name} SOAP action "
-					"must be either 'None' or a subclass of 'MessageBody'."
-				)
+		_validate_a8n_key(key, name)
+		if key == "return":
+			_validate_a8n_return(value, name)
 		elif key == "body":
-			if value == XroadHeader:
-				raise TypeError(
-					f"Cannot set the 'XroadHeader' class as a type annotation "
-					f"to the 'body' parameter of the {name} SOAP action."
-				)
-			elif not isinstance(value, type) or not issubclass(value, MessageBody):
-				raise TypeError(
-					f"The annotation of the '{key}' parameter of the {name} "
-					f"SOAP action must be a subclass of 'MessageBody'."
-				)
-		elif key == "header" and value != XroadHeader:
-			raise ValueError(
-				f"The annotation of the 'headers' parameter of the {name} "
-				f"SOAP action must be the 'XroadHeaders' class."
-			)
+			_validate_a8n_body(key, value, name)
+		elif key == "header":
+			_validate_a8n_header(value, name)
 	return anno
+
+
+def _validate_a8n_key(key: str, name: str) -> None:
+	if key not in ["body", "header", "return"]:
+		raise ValueError(
+			f"Parameter name '{key}' not allowed for SOAP action {name}."
+			"\nOnly names 'body' and 'header' can be used for parameters."
+		)
+
+
+def _validate_a8n_return(value: t.Any, name: str) -> None:
+	if value is None:
+		return
+	if not isinstance(value, type) or not issubclass(value, MessageBody):
+		raise TypeError(
+			f"Return type annotation of the {name} SOAP action "
+			"must be either 'None' or a subclass of 'MessageBody'."
+		)
+
+
+def _validate_a8n_body(key: str, value: t.Any, name: str) -> None:
+	if value == XroadHeader:
+		raise TypeError(
+			f"Cannot set the 'XroadHeader' class as a type annotation "
+			f"to the 'body' parameter of the {name} SOAP action."
+		)
+	elif not isinstance(value, type) or not issubclass(value, MessageBody):
+		raise TypeError(
+			f"The annotation of the '{key}' parameter of the {name} "
+			f"SOAP action must be a subclass of 'MessageBody'."
+		)
+
+
+def _validate_a8n_header(value: t.Any, name: str) -> None:
+	if value != XroadHeader:
+		raise ValueError(
+			f"The annotation of the 'headers' parameter of the {name} "
+			f"SOAP action must be the 'XroadHeaders' class."
+		)
 
 
 def extract_parameter_positions(anno: dict) -> dict:
